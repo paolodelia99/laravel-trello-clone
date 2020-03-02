4e9fda76b127c709d21f815ecb053b14ee666d1e
Paolo D'Elia <paolo.delia99@gmail.com>
2020-03-02 17:11:08 +0100

    Starting on working on the backend

---
+++ app/Task.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Task extends Model
+{
+    use SoftDeletes;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['name', 'category_id', 'user_id', 'order'];
+
+    public function category() {
+        return $this->hasOne(Category::class);
+    }
+
+    public function user() {
+        return $this->belongsTo(User::class);
+    }
+}
