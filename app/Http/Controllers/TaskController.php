a31338597c5becf853048e79133f5c654c186f90
Paolo D'Elia <paolo.delia99@gmail.com>
2020-03-02 17:29:22 +0100

    Before testing routes

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -35,7 +35,20 @@ class TaskController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $task = Task::create([
+            'name' => $request->name,
+            'category_id' => $request->category_id,
+            'user_id' => $request->user_id,
+            'order' => $request->order
+        ]);
+
+        $data = [
+            'data' => $task,
+            'status' => (bool) $task,
+            'message' => $task ? 'Task Created!' : 'Error Creating Task',
+        ];
+
+        return response()->json($data);
     }
 
     /**
--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,12 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::post('login', 'UserController@login');
+Route::post('register', 'UserController@register');
+
+
+Route::group(['middleware' => 'auth:api'], function(){
+    Route::resource('/task', 'TasksController');
+    Route::resource('/category', 'CategoryController');
+    Route::get('/category/{category}/tasks', 'CategoryController@tasks');
 });
