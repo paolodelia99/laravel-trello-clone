f7444b01fcc0068b3d180a571be31dbbee6d6a5a
Paolo D'Elia <paolo.delia99@gmail.com>
2020-03-02 22:26:17 +0100

    Set up vue and Starting finishing the controlles

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,24 @@
-require('./bootstrap');
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+Vue.use(VueRouter)
+
+import App from './components/App'
+import Welcome from './components/Welcome'
+
+const router = new VueRouter({
+    mode: 'history',
+    routes: [
+        {
+            path: '/',
+            name: 'home',
+            component: Welcome
+        },
+    ],
+});
+
+const app = new Vue({
+    el: '#app',
+    components: { App },
+    router,
+});
